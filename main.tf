d916ab78336225e7420694a0a20af34c5aee7974
mddeepu8 <84235132+mddeepu8@users.noreply.github.com>
2022-05-16 17:16:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,29 +9,26 @@ terraform {
 provider "github" {
   organization = "SynergyDevops"
   #version = "~> 4.0"
-  owner = "my-org"
 }
 
-resource "github_branch_protection_v3" "example" {
-  repository     = github_repository.example.test3
-  branch         = "main"
+resource "github_branch_protection_v3" "main_protection" {
+  count = length(var.repository)
+  repository =  var.repository[count.index]
+  #repository = "${var.repository}"
+  branch = "main"
   enforce_admins = true
   require_signed_commits = true
-
+  #require_conversation_resolution = true
+  #require_linear_history = true
+  #allows_deletions = true
+  
   required_status_checks {
     strict   = false
     contexts = ["ci/travis"]
   }
-
   required_pull_request_reviews {
-    dismiss_stale_reviews = true
-    dismissal_users       = ["test3-user"]
-    dismissal_teams       = [github_team.example.slug]
+    dismiss_stale_reviews  = true
     required_approving_review_count = 2
     require_code_owner_reviews = false
-  }
-
-resource "github_repository" "example" {
-  name = "test3"
   }
 }
