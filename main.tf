4b54344c4fc5211d0fbe7eef15f44a1c22125b2a
mddeepu8 <84235132+mddeepu8@users.noreply.github.com>
2022-05-16 16:49:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,24 +12,39 @@ provider "github" {
   owner = "my-org"
 }
 
-resource "github_branch_protection_v3" "main_protection" {
-  count = length(var.repository)
-  repository =  var.repository[count.index]
-  #repository = "${var.repository}"
-  branch = "main"
+resource "github_branch_protection_v3" "example" {
+  repository     = github_repository.example.name
+  branch         = "main"
   enforce_admins = true
-  require_signed_commits = true
-  #require_conversation_resolution = true
-  #require_linear_history = true
-  #allows_deletions = true
-  
+
   required_status_checks {
     strict   = false
     contexts = ["ci/travis"]
   }
+
   required_pull_request_reviews {
-    dismiss_stale_reviews  = true
-    required_approving_review_count = 2
-    require_code_owner_reviews = false
+    dismiss_stale_reviews = true
+    dismissal_users       = ["foo-user"]
+    dismissal_teams       = [github_team.example.slug]
   }
+
+  restrictions {
+    users = ["foo-user"]
+    teams = [github_team.example.slug]
+    apps  = ["foo-app"]
+  }
+}
+
+resource "github_repository" "example" {
+  name = "example"
+}
+
+resource "github_team" "example" {
+  name = "test3"
+}
+
+resource "github_team_repository" "example" {
+  team_id    = github_team.example.id
+  repository = github_repository.example.name
+  permission = "pull"
 }
