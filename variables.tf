bcde4d0eff7f3eb16a9a7c4e4d0383a7e1e4b141
mddeepu8 <84235132+mddeepu8@users.noreply.github.com>
2022-05-17 15:04:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,5 @@
-locals {
-  repository = toset([
-    "test1",
-    "test2",
-    "test3,
-  ])
+variable "repository" {
+  description = "Synergy Repositories"
+  type        = list(string)
+    default     = ["test1", "test2", "test3"]
 }
