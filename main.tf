d52f3edca89f6e67842a61bdbc929deecf32fe7b
mddeepu8 <84235132+mddeepu8@users.noreply.github.com>
2022-05-17 15:07:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "github" {
 }
 resource "github_branch_protection_v3" "main_protection" {
   for_each = toset( ["test1", "test2", "test3"] )
-  #repository = "${var.repository}"
+  repository = "${var.repository}"
   branch = "main"
   enforce_admins = true
   require_signed_commits = true
