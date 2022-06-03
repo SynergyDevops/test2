184b306bbdd2e917028579cbc761c5d149029252
mddeepu8 <84235132+mddeepu8@users.noreply.github.com>
2022-06-03 12:57:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,10 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "4.3.0"
-    }
-  }
+module "config" {
+  source  = "cloudposse/config/yaml"
+  version = "1.0.1"
 }
+  
 provider "github" {
   organization = "SynergyDevops"
   #version = "~> 4.0"
 }
-resource "github_branch_protection_v3" "main_protection" {
-  #for_each = toset( ["test1", "test2", "test3"] )
-  repository = "${var.repository}"
-  branch = "main"
-  enforce_admins = true
-  require_signed_commits = true
-  #require_conversation_resolution = true
-  #require_linear_history = true
-  #allows_deletions = true
-  
-  required_status_checks {
-    strict   = false
-    contexts = ["ci/travis"]
-  }
-  required_pull_request_reviews {
-    dismiss_stale_reviews  = true
-    required_approving_review_count = 2
-    require_code_owner_reviews = false
-  }
-}
+
